ba442553b436b0b39c1bae39773fa6d14a37438e
Andrew <andrewcgraves@gmail.com>
2022-12-31 02:02:07 -0800

    snow !

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useCallback } from 'react';
+import Particles from 'react-particles';
+import { tsParticles } from 'tsparticles-engine'
+import { loadFull } from 'tsparticles'
+import type { Container, Engine } from 'tsparticles-engine'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine)
+  }, [])
+
+  const date = new Date()
+  const isSnowing = date.getMonth() == 11
+  if (isSnowing) {
+    tsParticles.loadJSON('tsparticles', 'snow.json')
+  }
+
+  return <>
+    <Component {...pageProps} />
+    {isSnowing && <Particles init={particlesInit}/>}
+  </>
 }
 export default MyApp
