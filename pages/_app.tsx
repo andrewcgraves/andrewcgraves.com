56b3813cef4b594941c4f5e1adcb046af22b9cc2
andrew (m) <andrewcgraves@gmail.com>
2023-05-02 17:34:23 -0700

    Added dev banner & fixed mobile phone color issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Particles from 'react-particles';
 import { tsParticles } from 'tsparticles-engine'
 import { loadFull } from 'tsparticles'
 import type { Container, Engine } from 'tsparticles-engine'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [hostname, setHostname] = useState('')
+
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine)
   }, [])
@@ -17,9 +19,16 @@ function MyApp({ Component, pageProps }: AppProps) {
     tsParticles.loadJSON('tsparticles', 'snow.json')
   }
 
-  return <>
-    <Component {...pageProps} />
-    {isSnowing && <Particles init={particlesInit}/>}
-  </>
+  useEffect(() => {
+    setHostname(window.location.hostname)
+  }, [])
+
+  return (
+    <div className='app'>
+      {hostname != 'andrewcgraves.com' ? (<div className='warningBanner'><b>{hostname}</b> is used for development. Please use <b>andrewcgraves.com</b>.</div>) : null }
+      <Component {...pageProps} />
+      {isSnowing && <Particles init={particlesInit}/>}
+    </div>
+  )
 }
 export default MyApp
