bb3e7354727103a97484d34eea16f97a6d77cfea
andrew (m) <andrewcgraves@gmail.com>
2025-10-08 23:20:16 -0400

    adding next photos and tweaking spacing

--- pages/ceramics.tsx
+++ pages/ceramics.tsx
@@ -3,6 +3,7 @@ import UniversalHeaderBar from '../src/Components/UniversalHeaderBar.js'
 
 import PhotoSwipeLightbox from 'photoswipe/lightbox'
 import 'photoswipe/style.css'
+import Image from 'next/image.js'
 
 interface Photo {
     src: string,
@@ -14,32 +15,32 @@ interface Photo {
 const photos: Array<Photo> = [
     {
         src: "https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f102cf27e30aa6989_d20250928_m175150_c004_v0402007_t0052_u01759081910219",
-        width: 200,
-        height: 100,
+        width: 4032,
+        height: 3024,
         caption: ""
     },
     {
         src: "https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f1071b9487835410b_d20250928_m175133_c004_v0402026_t0024_u01759081893909",
-        width: 200,
-        height: 3,
+        width: 6240,
+        height: 4160,
         caption: ""
     },
     {
         src: "https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f10786810c074c97b_d20250928_m175136_c004_v0402000_t0057_u01759081896368",
-        width: 2,
-        height: 3,
+        width: 4032,
+        height: 3024,
         caption: ""
     },
     {
         src: "https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f107d8a16ace0d755_d20250928_m175144_c004_v0402029_t0054_u01759081904111",
-        width: 2,
-        height: 3,
+        width: 6240,
+        height: 4160,
         caption: ""
     },
     {
         src: "https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f1197e503498e592d_d20250928_m175148_c004_v0402026_t0003_u01759081908161",
-        width: 2,
-        height: 3,
+        width: 4032,
+        height: 3024,
         caption: ""
     },
     {
@@ -50,8 +51,8 @@ const photos: Array<Photo> = [
     },
     {
         src: "https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f101f76678969f1dc_d20250928_m175145_c004_v0402028_t0032_u01759081905173",
-        width: 2,
-        height: 3,
+        width: 3024,
+        height: 4032,
         caption: ""
     },
 ]
@@ -82,21 +83,28 @@ export default function Ceramics() {
                 <div className='main-container'>
                     <h1>Ceramics: IN PROGRESS</h1>
                     <p>Ceramics has become a big part of my life in the last few years. I teach and produce work with CSU Channel Islands, where I graduated. Ceramics is one of the ways I unwind, and express my creativity. Here are some of my favorite pieces.</p>
-                    <div id="image-gallery" className="pwsp-gallery grid grid-cols-2 md:grid-cols-3 gap-4 pt-8">
-                        {photos.map(photo => {
-                            return (
-                                // <a href={photo.src}
-                                // data-pswp-width={photo.width}
-                                // data-pwsp-height={photo.height}
-                                // data-cropped="true"
-                                // target="_blank"
-                                // rel="noreferrer"
-                                // >
-                                //     <img src={photo.src}></img>
-                                // </a>
-                                <img className="max-w-full object-cover object-center" src={photo.src}></img>
-                            )
-                        })}
+                    <div id="image-gallery" className="pwsp-gallery grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-8">
+                        <div>
+                            {photos.map((photo, index) => {
+                                if (index % 3 == 0) {
+                                    return <Image className="photo-grid-photo" id={photo.src} src={photo.src} alt={photo.caption} width={photo.width} height={photo.height}></Image>
+                                }
+                            })}
+                        </div>
+                        <div>
+                            {photos.map((photo, index) => {
+                                if (index % 3 == 1) {
+                                    return <Image className="photo-grid-photo" id={photo.src} src={photo.src} alt={photo.caption} width={photo.width} height={photo.height}></Image>
+                                }
+                            })}
+                        </div>
+                        <div>
+                            {photos.map((photo, index) => {
+                                if (index % 3 == 2) {
+                                    return <Image className="photo-grid-photo" id={photo.src} src={photo.src} alt={photo.caption} width={photo.width} height={photo.height}></Image>
+                                }
+                            })}
+                        </div>
                     </div>
                 </div>
             </main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import type { AppProps } from 'next/app'
-// import styles from '../styles/Home.module.css'
 import LinkBox from '../src/Components/LinkBox.js'
 import UniversalHeaderBar from '../src/Components/UniversalHeaderBar.js'
-import Pill from '../src/Components/Pill.js'
 
 // images
 import linkedInLogo from '../public/linkedInLogo.png'
@@ -22,7 +18,7 @@ export default function Home() {
       <main>
         <UniversalHeaderBar/>
         <div className="flex flex-wrap gap-4 p-16">
-          <div className="flex-initial lg:flex-1">
+          <div className="flex-initial lg:flex-2">
             <h1>
               Hello, I'm Andrew Graves
             </h1>
@@ -30,8 +26,7 @@ export default function Home() {
               I’m a full-stack developer at PennyMac Loan Services and a part time tinkerer. In my free time, I like being out in nature, creating ceramic art, designing and building things, and advocating for better regional transportation for California. <br/><br/>I’m most interested in how technology can augment people’s lives in meaningful ways and how we can leverage that to bring people together and make positive change. 
             </h2>
           </div>
-          <div className="flex-initial lg:flex-1 flex gap-4 sm:flex-row flex-wrap content-center justify-center">
-            {/* <img className="rounded-2xl" src="https://f004.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z8f907fb55c4c22b29911051f_f106e8302bae80c5f_d20250928_m203123_c004_v0402028_t0032_u01759091483421"></img> */}
+          <div className="flex-initial lg:flex-1 flex gap-4 sm:flex-row flex-wrap content-center justify-center rounded">
               <LinkBox link="https://www.linkedin.com/in/andrewcgraves/" imgSrc={linkedInLogo} size={24}/>
               <LinkBox link="https://github.com/andrewcgraves" imgSrc={githubLogo} size={24}/>
           </div>
