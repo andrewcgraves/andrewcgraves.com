9ca1a7f25db64fa8040f6d77a133a319a260ba73
Andrew <andrew@andrewcgraves.com>
2026-05-09 14:17:35 -0700

    feat: adding header selection, removing useless pages, page meta

--- src/Components/UniversalHeaderBar.js
+++ src/Components/UniversalHeaderBar.js
@@ -1,22 +1,38 @@
-import Pill from "./Pill"
+"use client";
 
-const defaultElements = [{name: "ceramics", isDisabled: false}, {name: "design", isDisabled: true}, {name: "projects", isDisabled: true}]
+import { usePathname } from "next/navigation";
+import Pill from "./Pill";
 
-export default function UniversalHeaderBar({children = null}) {
-    return (
-        <div className='p-4 w-fit flex gap-8 flex-wrap'>
-            <a href="/">
-                <img className='w-10' src='/website-large-dark.svg' />
-            </a>
-            {children == null ? defaultElements.map(element => {
-                let link = `/${element['name']}`
+const defaultElements = [
+  { name: "projects", isDisabled: false },
+  { name: "ceramics", isDisabled: false },
+];
 
-                return (
-                    <Pill link={link} isDisabled={element.isDisabled}>
-                        <p>{element['name'].toUpperCase()}</p>
-                    </Pill>
-                )
-            }): children}
-        </div>
-    )
+export default function UniversalHeaderBar({ children = null }) {
+  const pathname = usePathname();
+
+  return (
+    <div className="p-4 w-fit flex gap-8 flex-wrap">
+      <a href="/">
+        <img className="w-10" src="/website-large-dark.svg" />
+      </a>
+      {children == null
+        ? defaultElements.map((element) => {
+            let link = `/${element["name"]}`;
+            const isActive = pathname === link;
+
+            return (
+              <Pill
+                key={element.name}
+                link={link}
+                isDisabled={element.isDisabled}
+                isActive={isActive}
+              >
+                <p>{element["name"].toUpperCase()}</p>
+              </Pill>
+            );
+          })
+        : children}
+    </div>
+  );
 }
